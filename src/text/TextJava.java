6b62f663ea1646667734d2023d6dd2f5f1fc7f6c
wpzy <596320147@qq.com>
2016-11-14 12:37:13 +0800

    测试

--- src/text/TextJava.java
+++ src/text/TextJava.java
@@ -1,8 +1,17 @@
 package text;
 
+import java.util.Scanner;
+
 public class TextJava {
 	public static void main(String[] args){
 		System.out.println("hello world");
+		Scanner scanner=new Scanner(System.in);
+		String line=scanner.nextLine();
+		System.out.println("line"+line);
+		char a =line.charAt(0);
+		if(java.lang.Character.isWhitespace(a) ){
+			System.out.println("yes");
+		}
 	}
 
 }
