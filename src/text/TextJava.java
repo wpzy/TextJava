7c77c9138642d7a034db86459167c7ba1bd98ce3
wpzy <596320147@qq.com>
2016-11-14 13:45:19 +0800

    测试

--- src/text/TextJava.java
+++ src/text/TextJava.java
@@ -1,17 +1,29 @@
 package text;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextJava {
 	public static void main(String[] args){
-		System.out.println("hello world");
-		Scanner scanner=new Scanner(System.in);
-		String line=scanner.nextLine();
-		System.out.println("line"+line);
-		char a =line.charAt(0);
-		if(java.lang.Character.isWhitespace(a) ){
-			System.out.println("yes");
+		getTokenByScan();
+	}
+	
+	/*
+	*simple java tokenizer
+	**/
+	public static void getTokenByScan(){
+		@SuppressWarnings("resource")
+		Scanner scanner=new Scanner("hello world this is the test to test the function "
+				+ "of scanner");
+		List<String> result=new ArrayList<String>();
+		while(scanner.hasNext()){
+			result.add(scanner.next());
 		}
+		for(String a:result){
+			System.out.println(a);
+		}
+		
 	}
 
 }
